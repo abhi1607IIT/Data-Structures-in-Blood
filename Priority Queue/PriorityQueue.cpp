de43c53915a95f83d56ab710739f117bce328e1d
abhi1607IIT <abhi.gupta.1607@gmail.com>
2018-09-10 09:35:53 +0530

    added inplace heap sort

--- Priority Queue/PriorityQueue.cpp
+++ Priority Queue/PriorityQueue.cpp
@@ -2,6 +2,70 @@
 using namespace std;
 
 
+
+
+void inplaceHeapSort(int input[], int n){
+
+	for (int i=0;i<n;i++){
+		int elementpos=i;
+		int parentpos=(elementpos-1)/2;
+		while(parentpos>=0){
+			if(input[elementpos]<input[parentpos]){
+				swap(input[elementpos],input[parentpos]);
+			}
+			else{
+				break;
+			}
+			elementpos = parentpos;
+			parentpos = (parentpos-1)/2;
+		}
+	}
+
+	for(int i=0;i<n;i++){
+		cout<<input[i]<<" ";
+	}
+	cout<<endl;
+
+	for(int i=0;i<n;i++){
+		swap(input[0],input[n-1-i]);
+		int elementpos = 0;
+		int leftchild = elementpos*2 +1;
+		int rightchild = leftchild + 1;
+		while(leftchild < n-i-1 && rightchild < n-i-1){
+			int replace = min(input[elementpos],min(input[leftchild],input[rightchild]));
+			if(replace == input[leftchild]){
+				swap(input[elementpos],input[leftchild]);
+				elementpos = leftchild;
+				leftchild = elementpos*2 +1;
+				rightchild = leftchild + 1;
+			}
+			else if(replace == input[rightchild]){
+				swap(input[elementpos],input[rightchild]);
+				elementpos = rightchild;
+				leftchild = elementpos*2 +1;
+				rightchild = leftchild + 1;
+			}
+			else{
+				break;
+			}
+		}
+
+		if(leftchild < n-i-1 && rightchild >= n-i-1 && input[elementpos] > input[leftchild]){
+			swap(input[elementpos],input[leftchild]);
+			elementpos = leftchild;
+			leftchild = elementpos*2 +1;
+			rightchild = leftchild + 1;
+		}
+
+	}
+	for(int i=0;i<n;i++){
+		cout<<input[i]<<" ";
+	}
+	cout<<endl;
+}
+
+
+
 int GetMin(vector<int>pq){
 	if(pq.size()>=1){
 		return pq[1];
@@ -28,24 +92,24 @@ void InsertElement(vector<int>& pq,int element){   //O(h)   where h is height of
 }
 
 void DeleteElement(vector<int>& pq,int index){
-	if(index>=pq.size()){
+	if(index>pq.size()){
 		return;
 	}
 	swap(pq[index],pq[pq.size()-1]);
 	pq.pop_back();
 	int elementpos = index;
 	int leftchild = elementpos*2;
-	int rightchild = elementpos*2 + 1;
-	while(leftchild < pq.size() || rightchild < pq.size()){
+	int rightchild = leftchild + 1;
+	while(leftchild < pq.size() && rightchild < pq.size()){
 		int replace = min(pq[elementpos],min(pq[leftchild],pq[rightchild]));
 		if(replace == pq[leftchild]){
-			swap(pq[elementpos],replace);
+			swap(pq[elementpos],pq[leftchild]);
 			elementpos = leftchild;
 			leftchild = elementpos*2;
-			rightchild = elementpos*2 + 1;
+			rightchild = leftchild + 1;
 		}
 		else if(replace == pq[rightchild]){
-			swap(pq[elementpos],replace);
+			swap(pq[elementpos],pq[rightchild]);
 			elementpos = rightchild;
 			leftchild = elementpos*2;
 			rightchild = elementpos*2 + 1;
@@ -68,19 +132,22 @@ int main(){
     pq.push_back(-1);
     int n;
     cin>>n;
+    int*arr= new int [n];
     for(int i=0;i<n;i++){
     	int num;
     	cin>>num;
+    	arr[i]=num;
     	InsertElement(pq,num);
     }
     for(int i=1;i<pq.size();i++){
     	cout<<pq[i]<<" ";
     }
     cout<<endl;
-
-    DeleteElement(pq,2);
+    inplaceHeapSort(arr,n);
+    DeleteElement(pq,1);
     for(int i=1;i<pq.size();i++){
     	cout<<pq[i]<<" ";
     }
     cout<<endl;
 }
+
